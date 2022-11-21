10ba425cf2bc02d073d9f58a0d52845f2f5d6ee7
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2022-11-21 14:37:30 +0530

    Time: 1 ms (81.02%), Space: 42 MB (91.29%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,25 +14,37 @@
  * }
  */
 class Solution {
-    int height(TreeNode root){
-        if(root == null)
-            return 0;
-        
-        int left = height(root.left);
-        int right = height(root.right);
-        int ans = Math.max(left, right) + 1;
-        return ans;
-    }
     public int diameterOfBinaryTree(TreeNode root) {
-        if(root == null)
-            return 0;
+        if(root==null)return 0;
+   
+        DiaPair ans=findDiameter(root);
         
-        int option1 = diameterOfBinaryTree(root.left);
-        int option2 = diameterOfBinaryTree(root.right);
-        int option3 = height(root.left) + height(root.right);
+        return ans.diameter;
+    }
+    
+    public DiaPair findDiameter(TreeNode root ){  //O(n)
+        if(root==null){
+            return new DiaPair(-1, 0);
+        }
         
-        int ans1 = Math.max(option1, option2);
-        int ans = Math.max(option3, ans1);
-        return ans;
+        DiaPair lhs=findDiameter(root.left);
+        DiaPair rhs=findDiameter(root.right);
+        
+        DiaPair myPair=new DiaPair();
+        myPair.diameter=Math.max(lhs.height+rhs.height+2, Math.max(lhs.diameter, rhs.diameter));
+        myPair.height=Math.max(lhs.height, rhs.height)+1;
+        
+        return myPair;
+    }
+}
+class DiaPair{
+    int height;
+    int diameter;
+    
+    public DiaPair(){}
+    
+    public DiaPair(int height, int diameter){
+        this.height=height;
+        this.diameter=diameter;
     }
 }
