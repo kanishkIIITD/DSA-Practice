e689072a0723adf99642f4ded2a7161eb5fb6250
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2024-09-18 12:04:35 +0530

    Time: 5 ms (70.7%), Space: 17.2 MB (55.19%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,30 +1,28 @@
-bool compare(int a, int b) {
-    string sa = to_string(a);
-    string sb = to_string(b);
-
-    // Custom comparison to check which concatenation is larger
-    if((sa + sb )> (sb + sa)){
-        return true;
-    }
-    return false;
-}
-
 class Solution {
 public:
     string largestNumber(vector<int>& nums) {
-        // Sort using the custom comparator
-        sort(nums.begin(), nums.end(), compare);
+        // Convert integers to strings
+        vector<string> array;
+        for (int num : nums) {
+            array.push_back(to_string(num));
+        }
 
-        // If the largest number after sorting is '0', the result is "0"
-        if (nums[0] == 0) {
+        // Custom comparator for sorting
+        sort(array.begin(), array.end(), [](const string &a, const string &b) {
+            return (b + a) < (a + b);
+        });
+
+        // Handle the case where the largest number is "0"
+        if (array[0] == "0") {
             return "0";
         }
 
-        string ans = "";
-        for (int num : nums) {
-            ans += to_string(num);
+        // Build the largest number from the sorted array
+        string largest;
+        for (const string &num : array) {
+            largest += num;
         }
 
-        return ans;
+        return largest;
     }
 };
