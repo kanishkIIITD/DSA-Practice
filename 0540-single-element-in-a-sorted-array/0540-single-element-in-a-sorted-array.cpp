d64f4e387f0b4ba9e633f566b3d147ca5337df73
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2024-09-19 14:31:48 +0530

    Time: 20 ms (43.3%), Space: 24.8 MB (84.56%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,18 +1,40 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        int s = 0;
+        // int s = 0;
+        // int e = nums.size()-1;
+        // int m = s + (e-s)/2;
+        // while(s < e){
+        //     if(m%2 == 1)
+        //         m--;
+        //     if(nums[m] == nums[m+1])
+        //         s = m+2;
+        //     else
+        //         e = m;
+        //     m = s + (e-s)/2;
+        // }
+        // return nums[m];
+
+        // Solution 2:
+        int s = 0; 
         int e = nums.size()-1;
-        int m = s + (e-s)/2;
-        while(s < e){
-            if(m%2 == 1)
-                m--;
-            if(nums[m] == nums[m+1])
-                s = m+2;
-            else
-                e = m;
-            m = s + (e-s)/2;
+        while(s <= e){
+            if(s == e)
+                return nums[s];
+            int m = s + (e-s)/2;
+            if(m%2 == 0){
+                if(nums[m] == nums[m+1])
+                    s = m+2;
+                else
+                    e = m;
+            }
+            else{
+                if(nums[m] == nums[m-1])
+                    s = m+1;
+                else
+                    e = m-1;
+            }
         }
-        return nums[m];
+        return -1;
     }
 };
