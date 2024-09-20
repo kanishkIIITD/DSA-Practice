142e41f0bb569dc6998857aae38c63bcb817a0b9
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2024-09-20 16:34:02 +0530

    Time: 23 ms (89.52%), Space: 33.9 MB (78.42%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.cpp
@@ -9,14 +9,40 @@ public:
             else 
                 high--;
         }
-        // return {arr.begin()+low, arr.begin()+high+1};
-        // vector<int> ans;
-        // for(int i = low; i <= high; i++)
-        //     ans.push_back(arr[i]);
-        // return ans;
         return vector<int>(arr.begin()+low, arr.begin()+high+1);
     }
+    vector<int> binarySearchMethod(vector<int>& arr, int k, int x){
+        int s = 0;
+        int e = arr.size()-1;
+        int high = e;
+        while(s <= e){
+            int m = s + (e-s)/2;
+            if(arr[m] >= x){
+                high = m;
+                e = m-1;
+            }
+            else if(x > arr[m])
+                s = m+1;
+            else 
+                e = m-1;
+        }
+
+        int low = high-1;
+        while(k--){
+            if(low < 0)
+                high++;
+            else if(high >= arr.size())
+                low--;
+            else if(x-arr[low] > arr[high]-x)
+                high++;
+            else 
+                low--;
+        }
+
+        return vector<int>(arr.begin()+low+1, arr.begin()+high);
+    }
     vector<int> findClosestElements(vector<int>& arr, int k, int x) {
-        return twoPointerMethod(arr, k, x);
+        // return twoPointerMethod(arr, k, x);
+        return binarySearchMethod(arr, k, x);
     }
 };
