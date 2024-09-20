0469faf18b219784b5185f99884cd45473687666
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2024-09-20 15:44:43 +0530

    Time: 11 ms (77.01%), Space: 16.1 MB (71.09%) - LeetHub

--- 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
+++ 0532-k-diff-pairs-in-an-array/0532-k-diff-pairs-in-an-array.cpp
@@ -1,29 +1,53 @@
 class Solution {
 public:
+    int binarySearch(vector<int>&nums, int start, int end, int target){
+        int s = start;
+        int e = end;
+        while(s <= e){
+            int m = s + (e-s)/2;
+            if(nums[m] == target)
+                return m;
+            else if(nums[m] > target)
+                e = m-1;
+            else 
+                s = m+1;
+        }
+        return -1;
+    }
     int findPairs(vector<int>& nums, int k) {
         sort(nums.begin(), nums.end());
         if(nums.size() == 1)
             return 0;
 
-        map<pair<int, int>, int> M;
-        int i = 0;
-        int j = 1;
-        int ans = 0;
-        while(j < nums.size()){
-            int diff = nums[j] - nums[i];
-            if(diff == k && i != j && M[{nums[i], nums[j]}] < 1){
-                M[{nums[i], nums[j]}] ++;
-                ans++;
-                i++;
-                j++;
-            }
-            else if(diff > k){
-                i++;
-            }
-            else{
-                j++;
-            }
+        // map<pair<int, int>, int> M;
+        // int i = 0;
+        // int j = 1;
+        // int ans = 0;
+        // while(j < nums.size()){
+        //     int diff = nums[j] - nums[i];
+        //     if(diff == k && i != j && M[{nums[i], nums[j]}] < 1){
+        //         M[{nums[i], nums[j]}] ++;
+        //         ans++;
+        //         i++;
+        //         j++;
+        //     }
+        //     else if(diff > k){
+        //         i++;
+        //     }
+        //     else{
+        //         j++;
+        //     }
+        // }
+        // return ans;
+
+        // Solution 2
+        set<pair<int, int>> ans;
+        for(int i = 0; i < nums.size()-1; i++){
+            int target = k + nums[i];
+            int index = binarySearch(nums, i+1, nums.size()-1, target);
+            if(index != -1)
+                ans.insert(pair(nums[i], nums[index]));
         }
-        return ans;
+        return ans.size();
     }
 };
