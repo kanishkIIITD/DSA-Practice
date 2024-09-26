7c081425b96561366b5497dd542746b30bd48ca9
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2024-09-26 15:25:42 +0530

    Time: 3 ms (47.29%), Space: 7.6 MB (10.7%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -4,24 +4,13 @@ public:
         int i = 0; 
         int j = s.length()-1;
         while(i <= j){
-            // if(!(s[i] >= 'a' && s[i] <= 'z') || !(s[i] >= 'A' && s[i] <= 'Z'))
-            //     i++;
-            // if(!(s[j] >= 'a' && s[j] <= 'z') || !(s[j] >= 'A' && s[j] <= 'Z'))
-            //     j--;
-            // if(i <= j){
-            //     // swap(s[i], s[j]);
-            //     char temp = s[i];
-            //     s[i] = s[j];
-            //     s[j] = temp;
-            //     i++;j--;
-            // }
-            if(((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z')) && ((s[j] >= 'a' && s[j] <= 'z') || (s[j] >= 'A' && s[j] <= 'Z'))){
+            if(isalpha(s[i]) && isalpha(s[j])){
                 char temp = s[i];
                 s[i] = s[j];
                 s[j] = temp;
                 i++;j--;
             }
-            else if(((s[i] >= 'a' && s[i] <= 'z') || (s[i] >= 'A' && s[i] <= 'Z'))){
+            else if(isalpha(s[i])){
                 j--;
             }
             else
