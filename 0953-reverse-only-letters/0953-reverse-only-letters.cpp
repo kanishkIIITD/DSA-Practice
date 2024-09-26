56c3056a276938a2f4e4a7a4bd5a3e6a716e3fc3
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2024-09-26 15:26:12 +0530

    Time: 0 ms (100%), Space: 7.5 MB (61.88%) - LeetHub

--- 0953-reverse-only-letters/0953-reverse-only-letters.cpp
+++ 0953-reverse-only-letters/0953-reverse-only-letters.cpp
@@ -5,9 +5,7 @@ public:
         int j = s.length()-1;
         while(i <= j){
             if(isalpha(s[i]) && isalpha(s[j])){
-                char temp = s[i];
-                s[i] = s[j];
-                s[j] = temp;
+                swap(s[i], s[j]);
                 i++;j--;
             }
             else if(isalpha(s[i])){
