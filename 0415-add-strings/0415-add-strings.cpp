057e10493a60252e905c6ffd058730b5abb3543f
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2024-10-18 14:00:24 +0530

    Time: 106 ms (5.01%), Space: 232.7 MB (5.29%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    string solve(string num1, int p1, string num2, int p2, int carry = 0){
+        // base case
+        if(p1 < 0 && p2 < 0){
+            if(carry != 0){
+                return string(1, carry + '0');
+            }
+            return "";
+        }
+
+        // 1 case solve
+        int n1 = (p1 >= 0 ? num1[p1] : '0') - '0';
+        int n2 = (p2 >= 0 ? num2[p2] : '0') - '0';
+        int sum = n1 + n2 + carry;
+        int digit = sum % 10;
+        carry = sum / 10;
+        string ans = "";
+        ans.push_back(digit + '0');
+
+        // RE
+        ans += solve(num1, p1-1, num2, p2-1, carry);
+
+        return ans;
+    }
+
+    string addStrings(string num1, string num2) {
+       string ans = solve(num1, num1.size()-1, num2, num2.size()-1);
+       reverse(ans.begin(), ans.end());
+       return ans; 
+    }
+};
