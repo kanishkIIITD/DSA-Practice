311f1a2d3b70ad1e6cbd3dabf257eeb9f67b065c
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-22 13:26:08 +0530

    Time: 3 ms (72.68%), Space: 15.1 MB (11.67%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,15 +1,31 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        // Brute Force
+        // vector<int> ans;
+        // for(int i = 0; i < nums.size()-1; i++){
+        //     int newTarget = target - nums[i];
+        //     for(int j = i+1; j < nums.size(); j++){
+        //         if(newTarget == nums[j]){
+        //             ans.push_back(i);
+        //             ans.push_back(j);
+        //         }
+        //     }
+        // }
+        // return ans;
+
+        // Better
         vector<int> ans;
-        for(int i = 0; i < nums.size()-1; i++){
-            int newTarget = target - nums[i];
-            for(int j = i+1; j < nums.size(); j++){
-                if(newTarget == nums[j]){
-                    ans.push_back(i);
-                    ans.push_back(j);
-                }
-            }
+        map<int, int> mp;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            int ele = nums[i];
+            int req = target - ele;
+            if(mp.find(req) != mp.end()){
+                ans = {mp[req], i};
+                return ans;
+            }   
+            mp[ele] = i;
         }
         return ans;
     }
