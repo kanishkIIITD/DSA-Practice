814b33c594d7566ed7b260c2363aa9cec6748206
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-22 12:51:27 +0530

    Time: 0 ms (100%), Space: 26 MB (80.17%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -17,24 +17,50 @@ public:
         // return ans;
 
         // Optimized
-        int n = height.size();
-        vector<int> prefixMax(n,0);
-        vector<int> suffixMax(n,0);
+        // int n = height.size();
+        // vector<int> prefixMax(n,0);
+        // vector<int> suffixMax(n,0);
 
-        prefixMax[0] = height[0];
-        suffixMax[n-1] = height[n-1];
+        // prefixMax[0] = height[0];
+        // suffixMax[n-1] = height[n-1];
 
-        for(int i = 1; i < n; i++){
-            prefixMax[i] = height[i] > prefixMax[i-1] ? height[i] : prefixMax[i-1];
-        }
-        for(int i = n-2; i >= 0; i--){
-            suffixMax[i] = height[i] > suffixMax[i+1] ? height[i] : suffixMax[i+1];
-        }
+        // for(int i = 1; i < n; i++){
+        //     prefixMax[i] = height[i] > prefixMax[i-1] ? height[i] : prefixMax[i-1];
+        // }
+        // for(int i = n-2; i >= 0; i--){
+        //     suffixMax[i] = height[i] > suffixMax[i+1] ? height[i] : suffixMax[i+1];
+        // }
         
-        int ans = 0;
+        // int ans = 0;
 
-        for(int i = 0; i < n; i++){
-            ans += min(prefixMax[i], suffixMax[i]) - height[i];
+        // for(int i = 0; i < n; i++){
+        //     ans += min(prefixMax[i], suffixMax[i]) - height[i];
+        // }
+        // return ans;
+
+        // Further optimized
+        int n = height.size();
+        int ans = 0;
+        int lmax = 0;
+        int rmax = 0;
+        int l = 0;
+        int r = n-1;
+
+        while(l < r){
+            if(height[l] <= height[r]){
+                if(height[l] >= lmax)
+                    lmax = height[l];
+                else
+                    ans += lmax - height[l];
+                l++;
+            }
+            else{
+                if(height[r] >= rmax)
+                    rmax = height[r];
+                else
+                    ans += rmax - height[r];
+                r--;
+            }
         }
         return ans;
     }
