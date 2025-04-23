f694697efe930a1df5a86a7d79909ae7e0daab1b
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-24 01:08:55 +0530

    Time: 15 ms (34.73%), Space: 34.9 MB (94.55%) - LeetHub

---
+++ 1056-capacity-to-ship-packages-within-d-days/1056-capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    int getSum(vector<int>&weights){
+        int sum = 0;
+        for(int i = 0; i < weights.size(); i++)
+            sum += weights[i];
+        return sum;
+    }
+
+    bool isTrue(vector<int>&weights, int mid, int days){
+        int count = 0;
+        int sum = 0;
+        int n = weights.size();
+        for(int i = 0; i < n; i++){
+            sum += weights[i];
+            if(sum <= mid)
+                continue;
+            else{
+                count++;
+                sum = weights[i];
+            }
+        }
+        if(sum <= mid)
+            count++;
+        // if(sum == weights[n-1])
+        //     count++;
+        if(count <= days)
+            return true;
+        else
+            return false;
+    }
+
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low = *max_element(weights.begin(), weights.end());
+        int high = getSum(weights);
+        while(low <= high){
+            int mid = low + (high - low) / 2;
+            if(isTrue(weights, mid, days))
+                high = mid-1;
+            else
+                low = mid+1;
+        }
+        return low;
+    }
+};
