dbd2af6d336cb2daa5d71e1d88e18bd4490196e1
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-23 16:21:34 +0530

    Time: 0 ms (100%), Space: 26.1 MB (68.13%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -16,23 +16,55 @@ public:
         // return nums[m];
 
         // Solution 2:
-        int s = 0; 
-        int e = nums.size()-1;
+        // int s = 0; 
+        // int e = nums.size()-1;
+        // while(s <= e){
+        //     if(s == e)
+        //         return nums[s];
+        //     int m = s + (e-s)/2;
+        //     if(m%2 == 0){
+        //         if(nums[m] == nums[m+1])
+        //             s = m+2;
+        //         else
+        //             e = m;
+        //     }
+        //     else{
+        //         if(nums[m] == nums[m-1])
+        //             s = m+1;
+        //         else
+        //             e = m-1;
+        //     }
+        // }
+        // return -1;
+
+        int n = nums.size();
+        if(n == 1)
+            return nums[0];
+        if(nums[0] != nums[1])
+            return nums[0];
+        if(nums[n-2] != nums[n-1])
+            return nums[n-1];
+        int s = 1;
+        int e = n-2;
         while(s <= e){
-            if(s == e)
-                return nums[s];
             int m = s + (e-s)/2;
-            if(m%2 == 0){
-                if(nums[m] == nums[m+1])
+            if(nums[m] != nums[m-1] && nums[m] != nums[m+1])
+                return nums[m];
+            if(m+1 < n && nums[m] == nums[m+1]){
+                if(m % 2 == 0)
                     s = m+2;
                 else
-                    e = m;
+                    e = m-1;
             }
             else{
-                if(nums[m] == nums[m-1])
-                    s = m+1;
+                if(m > 0){
+                    if(m % 2 == 0)
+                    e = m-2;
                 else
-                    e = m-1;
+                    s = m+1;
+                }
+                else
+                    break;
             }
         }
         return -1;
