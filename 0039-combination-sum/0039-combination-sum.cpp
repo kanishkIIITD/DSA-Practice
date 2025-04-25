a64687eedd8dc3f69dcf19ed52d800fae579a84d
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-25 12:38:20 +0530

    Time: 0 ms (100%), Space: 13.9 MB (91.44%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void getCombinations(vector<int>& candidates, int target, vector<int>& temp, vector<vector<int>>& ans, int i){
+        if(i >= candidates.size()){
+            if(target == 0)
+                ans.push_back(temp);
+            return;
+        }
+        
+        if(candidates[i] <= target){
+            // Pick
+            temp.push_back(candidates[i]);
+            getCombinations(candidates, target - candidates[i], temp, ans, i);
+            temp.pop_back();
+        }
+        // Not Pick
+        getCombinations(candidates, target, temp, ans, i+1);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> temp;
+        getCombinations(candidates, target, temp, ans, 0);
+        return ans;
+    }
+};
