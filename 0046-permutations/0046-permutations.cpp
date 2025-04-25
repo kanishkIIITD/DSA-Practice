5c35ece242d77cd89a2fd98eccfd494bcccd9aab
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-25 18:15:28 +0530

    Time: 0 ms (100%), Space: 10.4 MB (98.07%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
-    void getPermutations(vector<int>& nums, int i, vector<int>& temp, vector<int>& vis, vector<vector<int>>& ans){
+    void getPermutations(vector<int>& nums, int i, vector<int>& temp, vector<vector<int>>& ans){
         if(i == nums.size()){
             ans.push_back(temp);
             return;
         }
-        for(int idx = 0; idx < nums.size(); idx++){
-            if(vis[idx] != 1){
-                temp.push_back(nums[idx]);
-                vis[idx] = 1;
-                getPermutations(nums, i+1, temp, vis, ans);
-                vis[idx] = 0;
-                temp.pop_back();
-            }
+
+        for(int idx = i; idx < nums.size(); idx++){
+            swap(nums[i], nums[idx]);
+            temp.push_back(nums[i]);
+            getPermutations(nums, i+1, temp, ans);
+            swap(nums[i], nums[idx]);
+            temp.pop_back();
         }
     }
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> ans;
         vector<int> temp;
-        vector<int> vis(nums.size(), 0); //0 - visited, 1 - not visited
-        getPermutations(nums, 0, temp, vis, ans);
+        // vector<int> vis(nums.size(), 0); //0 - visited, 1 - not visited
+        // getPermutations(nums, 0, temp, vis, ans);
+        getPermutations(nums, 0, temp, ans);
         return ans;
     }
 };
