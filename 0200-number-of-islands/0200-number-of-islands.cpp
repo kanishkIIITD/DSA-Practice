070abd76ea7ce9f4aa7ce50d1018cd30c8eaae7a
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-28 22:41:21 +0530

    Time: 62 ms (11.83%), Space: 25.6 MB (13.45%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -23,13 +23,28 @@ public:
             }
         }
     }
+    void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
+        vis[row][col] = 1;
+        vector<int> deltaRow = {-1, 0, 1, 0};
+        vector<int> deltaCol = {0, 1, 0, -1};
+        for(int k = 0; k < 4; k++){
+            int nRow = row + deltaRow[k];
+            int nCol = col + deltaCol[k];
+            if(0 <= nRow && nRow < grid.size() 
+            && 0 <= nCol && nCol < grid[0].size() 
+            && grid[nRow][nCol] == '1' && !vis[nRow][nCol]){
+                dfs(nRow, nCol, vis, grid);
+            }
+        }
+    }
     int numIslands(vector<vector<char>>& grid) {
         vector<vector<int>> vis(grid.size(), vector<int>(grid[0].size(), 0));
         int islands = 0;
         for(int i = 0; i < grid.size(); i++){
             for(int j = 0; j < grid[0].size(); j++){
                 if(grid[i][j] == '1' && !vis[i][j]){
-                    bfs(i, j, vis, grid);
+                    // bfs(i, j, vis, grid);
+                    dfs(i, j, vis, grid);
                     islands++;
                 }
             }
