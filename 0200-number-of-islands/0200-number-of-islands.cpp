6f753d15f8a3b6da35f739358bdb558980e232f4
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-28 23:01:49 +0530

    Time: 29 ms (45.63%), Space: 17.4 MB (51.77%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,12 +1,15 @@
 class Solution {
 public:
+        vector<int> deltaRow = {-1, 0, 1, 0};
+        vector<int> deltaCol = {0, 1, 0, -1};
     void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
         queue<pair<int, int>> q;
         q.push({row,col});
         vis[row][col] = 1;
         while(!q.empty()){
-            int i = q.front().first;
-            int j = q.front().second;
+            // int i = q.front().first;
+            // int j = q.front().second;
+            auto [i,j] = q.front();
             q.pop();
             
             vector<int> deltaRow = {-1, 0, 1, 0};
@@ -25,8 +28,7 @@ public:
     }
     void dfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>>& grid){
         vis[row][col] = 1;
-        vector<int> deltaRow = {-1, 0, 1, 0};
-        vector<int> deltaCol = {0, 1, 0, -1};
+
         for(int k = 0; k < 4; k++){
             int nRow = row + deltaRow[k];
             int nCol = col + deltaCol[k];
