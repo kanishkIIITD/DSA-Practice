4a1bda94508e4c02096c243e933d5c36f18ad903
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-30 00:28:31 +0530

    Time: 0 ms (100%), Space: 7.8 MB (87.1%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -8,13 +8,21 @@ public:
     //     return dp[n] = solve(n-1, dp) + solve(n-2, dp);
     // }
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
+        // vector<int> dp(n+1, -1);
         // return solve(n, dp);
-        dp[0] = 1;
-        dp[1] = 1;
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // for(int i = 2; i <= n; i++){
+        //     dp[i] = dp[i-1] + dp[i-2];
+        // }
+        // return dp[n];
+        int prev2 = 1;
+        int prev = 1;
         for(int i = 2; i <= n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
         }
-        return dp[n];
+        return prev;
     }
 };
