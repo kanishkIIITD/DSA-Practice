7f9180fb378789e3b8400f9e71eadd50c70be111
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-29 22:20:14 +0530

    Time: 6 ms (62.2%), Space: 34.9 MB (88.82%) - LeetHub

--- 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.cpp
@@ -1,3 +1,38 @@
+// class Solution {
+// public:
+//     int countPaths(int n, vector<vector<int>>& roads) {
+//         vector<vector<pair<int, int>>> adj(n);
+//         for(auto road: roads){
+//             adj[road[0]].push_back({road[1], road[2]});
+//             adj[road[1]].push_back({road[0], road[2]});
+//         }
+//         int mod = (int)(1e9 + 7);
+//         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+//         vector<int> ways(n, 0);
+//         vector<int> dist(n, 1e9);
+//         pq.push({0, 0});
+//         ways[0] = 1;
+//         dist[0] = 0;
+//         while(!pq.empty()){
+//             int d = pq.top().first;
+//             int node = pq.top().second;
+//             pq.pop();
+//             for(auto it: adj[node]){
+//                 int adjNode = it.first;
+//                 int w = it.second;
+//                 if(d + w < dist[adjNode]){
+//                     dist[adjNode] = d + w;
+//                     pq.push({dist[adjNode], adjNode});
+//                     ways[adjNode] = ways[node];
+//                 }
+//                 else if(d + w == dist[adjNode]){
+//                     ways[adjNode] = (ways[adjNode] + ways[node]) % mod;
+//                 }
+//             }
+//         }
+//         return ways[n-1] % mod;
+//     }
+// };
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
