b9a830d37c5fc1d64a61bd098d5969abf4bf18d9
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-29 15:22:26 +0530

    Time: 0 ms (100%), Space: 17.3 MB (61.81%) - LeetHub

--- 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
+++ 0801-is-graph-bipartite/0801-is-graph-bipartite.cpp
@@ -18,11 +18,27 @@ public:
         }
         return true;
     }
+    bool dfs(int node, int color, vector<int>& vis, vector<vector<int>>& graph) {
+        vis[node] = color;
+        for(int neighbor : graph[node]) {
+            if(vis[neighbor] == -1) {
+                // Propagate failure upward immediately
+                if(!dfs(neighbor, !color, vis, graph)) {
+                    return false;
+                }
+            }
+            else if(vis[neighbor] == color) {
+                return false; // Direct conflict
+            }
+        }
+        return true; // All neighbors validated
+    }
     bool isBipartite(vector<vector<int>>& graph) {
         vector<int> vis(graph.size(), -1);
         for(int i = 0; i < graph.size(); i++){
             if(vis[i] == -1)
-                if(!bfs(i, vis, graph))
+                // if(!bfs(i, vis, graph))
+                if(!dfs(i, 0, vis, graph))
                     return false;
         }
         return true;
