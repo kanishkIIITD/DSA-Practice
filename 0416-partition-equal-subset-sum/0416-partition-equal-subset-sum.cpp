79dcc7bd54617b138c022d9db60d3c6917188653
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-30 15:10:21 +0530

    Time: 107 ms (50.26%), Space: 110.4 MB (15.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -18,8 +18,22 @@ public:
         int n = nums.size();
         if(sum % 2 != 0)
             return false;
-        vector<vector<int>> dp(n, vector<int>(sum/2+1, -1));
-        return solve(n-1, sum/2, nums, dp);
+        // vector<vector<int>> dp(n, vector<int>(sum/2+1, -1));
+        // return solve(n-1, sum/2, nums, dp);
 
+        vector<vector<int>> dp(n, vector<int>(sum/2+1, 0));
+        for(int i = 0; i < n; i++){
+            dp[i][0] = true;
+        }
+        if(nums[0] <= sum/2)
+            dp[0][nums[0]] = true;
+        for(int index = 1; index < n; index++){
+            for(int target = 1; target < sum/2+1; target++){
+                bool notTake = dp[index-1][target];
+                bool take = target >= nums[index] ? dp[index-1][target - nums[index]] : false;
+                dp[index][target] = take || notTake;
+            }
+        }
+        return dp[n-1][sum/2];
     }
 };
