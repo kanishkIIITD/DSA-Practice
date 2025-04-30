28f7382e36949f8b76a19ad9b158eef383004d5e
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-30 11:32:29 +0530

    Time: 0 ms (100%), Space: 10.5 MB (62.48%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -12,7 +12,17 @@ public:
         return dp[i] = max(opt1, opt2);
     }
     int rob(vector<int>& nums) {
-        vector<int> dp(nums.size() + 2, -1);
+        int n = nums.size();
+        vector<int> dp(n + 2, -1);
         return solve(0, nums, dp);
+
+        dp[n] = 0;
+        dp[n+1] = 0;
+        for(int i = n-1; i >= 0; i--){
+            int opt1 = nums[i] + dp[i+2];
+            int opt2 = dp[i+1];
+            dp[i] = max(opt1, opt2);
+        }
+        return dp[0];
     }
 };
