973732bd394644cf0c81198bd2ca591095046d0c
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-30 17:06:40 +0530

    Time: 0 ms (100%), Space: 97.2 MB (87.1%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,24 +1,13 @@
 class Solution {
 public:
-    void solveRE(vector<int>&prices, int i, int& minPrice, int& maxProfit){
-        // base case
-        if(i >= prices.size())
-            return;
-
-        // 1 case solution  
-        if(prices[i] < minPrice)
-            minPrice = prices[i];
-        if(prices[i] - minPrice > maxProfit)
-            maxProfit = prices[i] - minPrice;
-
-        // RE
-        solveRE(prices, i+1, minPrice, maxProfit);
-
-    }
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX;
-        int maxProfit = INT_MIN;
-        solveRE(prices, 0, minPrice, maxProfit);
-        return maxProfit;
+        int profit = 0;
+        int mini = prices[0];
+        for(int i = 1; i < prices.size(); i++){
+            int cost = prices[i] - mini;
+            profit = max(profit, cost);
+            mini = min(mini, prices[i]);
+        }
+        return profit;
     }
 };
