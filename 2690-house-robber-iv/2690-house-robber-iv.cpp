6e846294226c869ab6a612f9ab04952838f087a0
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-04-30 12:21:17 +0530

    Time: 53 ms (5.17%), Space: 115.1 MB (5%) - LeetHub

--- 2690-house-robber-iv/2690-house-robber-iv.cpp
+++ 2690-house-robber-iv/2690-house-robber-iv.cpp
@@ -1,41 +1,22 @@
 class Solution {
 public:
-    // bool canRob(vector<int>& nums, int mid, int k) {
-    //     int count = 0, n = nums.size();
-    //     for (int i = 0; i < n; i++) {
-    //         if (nums[i] <= mid) {
-    //             count++;
-    //             i++;
-    //         }
-    //     }
-    //     return count >= k;
-    // }
-
-    bool canRob(int mid, int k, vector<int>&nums){
-        int count = 0;
+    bool canRob(int mid, int k, vector<int>& nums) {
         int n = nums.size();
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= mid) {
-                count++;
-                i++;
-            }
+        vector<int> dp(n + 1);
+        dp[0] = 0;
+        dp[1] = (nums[0] <= mid) ? 1 : 0;
+        
+        for (int i = 2; i <= n; i++) {
+            dp[i] = max(
+                dp[i-1],  // Skip current house
+                (nums[i-1] <= mid) ? dp[i-2] + 1 : 0  // Rob current house if valid
+            );
         }
-        return count >= k;
+        return dp[n] >= k;
     }
 
-    int minCapability(vector<int>& nums, int k) {
-        // int left = 1, right = *max_element(nums.begin(), nums.end()), ans = right;
-        // while (left <= right) {
-        //     int mid = (left + right) / 2;
-        //     if (canRob(nums, mid, k)) {
-        //         ans = mid;
-        //         right = mid - 1;
-        //     } else {
-        //         left = mid + 1;
-        //     }
-        // }
-        // return ans;
 
+    int minCapability(vector<int>& nums, int k) {
         int low = 1;
         int high = *max_element(nums.begin(), nums.end());
         int ans = INT_MAX;
