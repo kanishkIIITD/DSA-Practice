d962481b41722bd98f60e6b42f644bdd2db886ad
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-05-01 11:41:12 +0530

    Time: 2 ms (82.58%), Space: 13.1 MB (84.06%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     void solve (int i, int k, string&s, vector<string>&ans, int n){
-        if(i == 2 * n && k == 0){
+        if(i == n && k == 0){
             ans.push_back(s);
             return;
         }
-        if(i+1 <= 2*n){
+        if(i+1 <= n){
             s += '(';
             solve(i+1, k+1, s, ans, n);
             s.pop_back();
         }
         if(k > 0){
             s += ')';
-            solve(i+1, k-1, s, ans, n);
+            solve(i, k-1, s, ans, n);
             s.pop_back();
         }
     }
