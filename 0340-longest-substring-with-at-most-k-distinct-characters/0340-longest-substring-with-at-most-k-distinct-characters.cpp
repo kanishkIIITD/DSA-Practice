2c33a548055863063140df46155eba8635b7eb01
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-05-16 21:55:58 +0530

    Time: 12 ms (7.79%), Space: 10.7 MB (52.35%) - LeetHub

--- 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -1,21 +1,26 @@
 class Solution {
 public:
-    int getNoOfChar(int hash[]){
-        int ans = 0;
-        for(int i = 0; i < 256; i++){
-            if(hash[i] > 0)
-                ans++;
-        }
-        return ans;
-    }
+    // int getNoOfChar(int hash[]){
+    //     int ans = 0;
+    //     for(int i = 0; i < 256; i++){
+    //         if(hash[i] > 0)
+    //             ans++;
+    //     }
+    //     return ans;
+    // }
     int lengthOfLongestSubstringKDistinct(string s, int k) {
-        int hash[256] = {0};
+        // int hash[256] = {0};
+        unordered_map<char, int> mp;
         int maxLen = 0;
         int l = 0;
         for(int r = 0; r < s.size(); r++){
-            hash[s[r]]++;
-            if(getNoOfChar(hash) > k){
-                hash[s[l]]--;
+            // hash[s[r]]++;
+            mp[s[r]]++;
+            // if(getNoOfChar(hash) > k)
+            while(mp.size() > k) {
+                mp[s[l]]--;
+                if(mp[s[l]] == 0)
+                    mp.erase(s[l]);
                 l++;
             }
             maxLen = max(maxLen, r-l+1);
