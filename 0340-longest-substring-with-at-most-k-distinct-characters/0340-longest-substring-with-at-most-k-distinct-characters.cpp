86d8ce281307d6a40d458d7400cd5d211e3e3fc9
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-05-16 21:53:46 +0530

    Time: 20 ms (5.64%), Space: 9.6 MB (92.08%) - LeetHub

---
+++ 0340-longest-substring-with-at-most-k-distinct-characters/0340-longest-substring-with-at-most-k-distinct-characters.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int getNoOfChar(int hash[]){
+        int ans = 0;
+        for(int i = 0; i < 256; i++){
+            if(hash[i] > 0)
+                ans++;
+        }
+        return ans;
+    }
+    int lengthOfLongestSubstringKDistinct(string s, int k) {
+        int hash[256] = {0};
+        int maxLen = 0;
+        int l = 0;
+        for(int r = 0; r < s.size(); r++){
+            hash[s[r]]++;
+            while(getNoOfChar(hash) > k){
+                hash[s[l]]--;
+                l++;
+            }
+            maxLen = max(maxLen, r-l+1);
+        }
+        return maxLen;
+    }
+};
