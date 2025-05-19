9c4f97b282c316370098635ebc8a2874776e6dc1
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-05-19 19:57:43 +0530

    Time: 0 ms (100%), Space: 8.4 MB (48.6%) - LeetHub

--- 0778-reorganize-string/0778-reorganize-string.cpp
+++ 0778-reorganize-string/0778-reorganize-string.cpp
@@ -2,38 +2,34 @@ class Solution {
 public:
     string reorganizeString(string s) {
         int hash[26] = {0};
-        for(int i = 0; i < s.length(); i++){
-            hash[s[i] - 'a']++;
+        for (char c : s) {
+            hash[c - 'a']++;
         }
 
-        int most_freq = INT_MIN;
-        char most_freq_char;
-        for(int i = 0; i < 26; i++){
-            if(hash[i] > most_freq){
-                most_freq = hash[i];
-                most_freq_char = i + 'a';
-            }
+        priority_queue<pair<int, char>> pq; // max-heap by freq
+        for (int i = 0; i < 26; i++) {
+            if (hash[i] != 0)
+                pq.push({hash[i], i + 'a'});
         }
 
-        int index = 0;
-        while(most_freq > 0 && index < s.length()){
-            s[index] = most_freq_char;
-            most_freq--;
-            index += 2;
+        string ans = "";
+        while (pq.size() > 1) {
+            auto [freq1, ch1] = pq.top(); pq.pop();
+            auto [freq2, ch2] = pq.top(); pq.pop();
+
+            ans += ch1; freq1--;
+            ans += ch2; freq2--;
+
+            if (freq1 > 0) pq.push({freq1, ch1});
+            if (freq2 > 0) pq.push({freq2, ch2});
         }
 
-        if(most_freq != 0)
-            return "";
-        
-        hash[most_freq_char - 'a'] = 0;
-        for(int i = 0; i < 26; i++){
-            while(hash[i] > 0){
-                index = index >= s.length() ? 1 : index;
-                s[index] = i + 'a';
-                hash[i]--;
-                index += 2;
-            }
+        if (!pq.empty()) {
+            auto [freq, ch] = pq.top(); pq.pop();
+            if (freq > 1) return "";
+            ans += ch;
         }
-        return s;
+
+        return ans;
     }
 };
