6040ac268d8fc767c962fcd00ac10a1af11532cc
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-05-19 14:24:00 +0530

    Time: 6 ms (34.22%), Space: 18.4 MB (65.88%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -8,32 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+class compare{
+    public:
+    bool operator()(ListNode* a, ListNode* b){
+        return a->val > b->val;
+    }
+};
 class Solution {
 public:
-    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
-        if (!l1) return l2;
-        if (!l2) return l1;
-
-        if (l1->val < l2->val) {
-            l1->next = mergeTwoLists(l1->next, l2);
-            return l1;
-        } else {
-            l2->next = mergeTwoLists(l1, l2->next);
-            return l2;
-        }
-    }
-
     ListNode* mergeKLists(vector<ListNode*>& lists) {
-        if (lists.empty()) return nullptr;
-        return divideAndConquer(lists, 0, lists.size() - 1);
-    }
-
-    ListNode* divideAndConquer(vector<ListNode*>& lists, int left, int right) {
-        if (left == right) return lists[left];
-
-        int mid = left + (right - left) / 2;
-        ListNode* l1 = divideAndConquer(lists, left, mid);
-        ListNode* l2 = divideAndConquer(lists, mid + 1, right);
-        return mergeTwoLists(l1, l2);
+        priority_queue<ListNode*, vector<ListNode*>, compare> pq;
+        for(int i = 0; i < lists.size(); i++){
+            if(lists[i])
+                pq.push(lists[i]);
+        }
+        ListNode* ans = new ListNode();
+        ListNode* temp = ans;
+        while(!pq.empty()){
+            ListNode* node = pq.top();
+            pq.pop();
+            temp->next = node;
+            temp = temp->next;
+            if(node->next)
+                pq.push(node->next);
+        }
+        return ans->next;
     }
 };
