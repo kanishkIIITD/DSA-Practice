92f9015488ba1a2ea7a5939762fd1185ff35dca1
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2025-05-19 15:32:58 +0530

    Time: 80 ms (59.88%), Space: 37.6 MB (64.84%) - LeetHub

--- 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.cpp
@@ -1,41 +1,31 @@
 class Solution {
 public:
     vector<int> smallestRange(vector<vector<int>>& nums) {
-        // Min-Heap: stores (value, list index, element index)
-        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> minHeap;
-        int curMax = numeric_limits<int>::min();
-
-        // Initialize the heap with the first element of each list
-        for (int i = 0; i < nums.size(); i++) {
-            minHeap.push({nums[i][0], i, 0});
-            curMax = max(curMax, nums[i][0]);
-        }
-        // Track the smallest range
-        vector<int> smallRange = {0, numeric_limits<int>::max()};
-
-        while (!minHeap.empty()) {
-            // Get the minimum element from the heap
-            vector<int> curr = minHeap.top();
-            minHeap.pop();
-            int curMin = curr[0], listIdx = curr[1], elemIdx = curr[2];
-
-            // Update the smallest range if a better one is found
-            if ((curMax - curMin < smallRange[1] - smallRange[0]) ||
-                (curMax - curMin == smallRange[1] - smallRange[0] && curMin < smallRange[0])) {
-                smallRange[0] = curMin;
-                smallRange[1] = curMax;
+        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<tuple<int, int, int>>> pq;
+        int maxi = INT_MIN, mini = INT_MAX;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i].size() > 0){
+                pq.push({nums[i][0], i, 0});
+                maxi = max(maxi, nums[i][0]);
+                mini = min(mini, nums[i][0]);
             }
-
-            // Move to the next element in the same list
-            if (elemIdx + 1 < nums[listIdx].size()) {
-                int nextVal = nums[listIdx][elemIdx + 1];
-                minHeap.push({nextVal, listIdx, elemIdx + 1});
-                curMax = max(curMax, nextVal);
-            } else {
-                // If any list is exhausted, stop
+        }
+        int ansStart = mini, ansEnd = maxi;
+        while(!pq.empty()){
+            auto [ele, i, j] = pq.top();
+            pq.pop();
+            mini = ele;
+            if(maxi - mini < ansEnd - ansStart){
+                ansStart = mini;
+                ansEnd = maxi;
+            }
+            if(j+1 < nums[i].size()){
+                pq.push({nums[i][j+1], i, j+1});
+                maxi = max(maxi, nums[i][j+1]);
+            }
+            else
                 break;
-            }
         }
-        return smallRange;
+        return {ansStart, ansEnd};
     }
 };
