05842e071fe0b347d91cf5fcc8a60da3004a0ac7
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-02-13 14:57:27 +0530

    Time: 0 ms (100%), Space: 23.6 MB (63.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,45 +1,39 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
-        int count1 = 0;
-        int count2 = 0;
-        int element1, element2;
-        int n = nums.size();
-        for(int i = 0; i < n; i++){
-            if(count1 == 0 && element2 != nums[i]){
+        int count1 = 0, count2 = 0, ele1 = INT_MIN, ele2 = INT_MIN;
+        for(int i = 0; i < nums.size(); i++){
+            if(count1 == 0 && ele2 != nums[i]){
                 count1++;
-                element1 = nums[i];
+                ele1 = nums[i];
             }
-            else if(count2 == 0 && element1 != nums[i]){
+            else if(count2 == 0 && ele1 != nums[i]){
                 count2++;
-                element2 = nums[i];
+                ele2 = nums[i];
             }
-            else if(element1 == nums[i]){
+            else if(ele1 == nums[i])
                 count1++;
-            }
-            else if(element2 == nums[i]){
+            else if(ele2 == nums[i])
                 count2++;
-            }
             else{
                 count1--;
                 count2--;
             }
         }
-        vector<int> ans;
-        int cnt1 = 0;
-        int cnt2 = 0;
-        for(int i = 0; i < n; i++){
-            if(nums[i] == element1)
-                cnt1++;
-            if(nums[i] == element2)
-                cnt2++;
+        count1 = 0, count2 = 0;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == ele1)
+                count1++;
         }
-        if(element1 == element2)
-            return {element1};
-        if(cnt1++ > floor(n/3))
-            ans.push_back(element1);
-        if(cnt2++ > floor(n/3))
-            ans.push_back(element2);
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] == ele2)
+                count2++;
+        }
+        vector<int> ans;
+        if(count1 > nums.size() / 3)
+            ans.push_back(ele1);
+        if(count2 > nums.size() / 3)
+            ans.push_back(ele2);
         return ans;
     }
 };
