1018836e38c0a61e7644ee55d0067353a96733f6
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-02-18 14:48:10 +0530

    Time: 1664 ms (5%), Space: 508 MB (5.01%) - LeetHub

---
+++ 0018-4sum/0018-4sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        set<vector<int>> st;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            for(int j = i+1; j < n; j++){
+                set<long long> hashset;
+                for(int k = j+1; k < n; k++){
+                    long long sum = nums[i] + nums[j];
+                    sum += nums[k];
+                    long long num = target - sum;
+                    if(hashset.find(num) != hashset.end()){
+                        vector<int> temp = {nums[i], nums[j], nums[k], (int)num};
+                        sort(temp.begin(), temp.end());
+                        st.insert(temp);
+                    }
+                    hashset.insert(nums[k]);
+                }
+            }
+        }
+        vector<vector<int>> ans(st.begin(), st.end());
+        return ans;
+    }
+};
