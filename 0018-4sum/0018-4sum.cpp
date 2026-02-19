39c41946821b70313f1f852a956d4e0434a74d4c
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-02-19 13:52:14 +0530

    Time: 23 ms (53.54%), Space: 17.6 MB (23.34%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -1,25 +1,43 @@
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        set<vector<int>> st;
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
         int n = nums.size();
         for(int i = 0; i < n; i++){
+            if(i > 0 && nums[i] == nums[i-1])
+                continue;
             for(int j = i+1; j < n; j++){
-                set<long long> hashset;
-                for(int k = j+1; k < n; k++){
+                if(j > i+1 && nums[j] == nums[j-1])
+                    continue;
+                int k = j+1, l = n-1;
+                while(k < l){
                     long long sum = nums[i] + nums[j];
                     sum += nums[k];
-                    long long num = target - sum;
-                    if(hashset.find(num) != hashset.end()){
-                        vector<int> temp = {nums[i], nums[j], nums[k], (int)num};
-                        sort(temp.begin(), temp.end());
-                        st.insert(temp);
+                    sum += nums[l];
+                    if(sum > target){
+                        l--;
+                        while(k < l && nums[l] == nums[l+1])
+                            l--;
                     }
-                    hashset.insert(nums[k]);
+                    else if(sum < target){
+                        k++;
+                        while(k < l && nums[k] == nums[k-1])
+                            k++;
+                    }
+                    else{
+                        vector<int> temp = {nums[i], nums[j], nums[k], nums[l]};
+                        ans.push_back(temp);
+                        k++;
+                        while(k < l && nums[k] == nums[k-1])
+                            k++;
+                        l--;
+                        while(k < l && nums[l] == nums[l+1])
+                            l--;
+                    }
                 }
             }
         }
-        vector<vector<int>> ans(st.begin(), st.end());
         return ans;
     }
 };
