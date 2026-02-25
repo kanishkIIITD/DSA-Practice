e30606f8fe7511cfbdaf7b47cc1470d555c59894
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-02-25 18:40:47 +0530

    Time: 7 ms (40.03%), Space: 23.7 MB (80.56%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -4,18 +4,11 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<vector<int>> ans;
         for(int i = 0; i < intervals.size(); i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!ans.empty() && end <= ans.back()[1])
-                continue;
-            for(int j = i+1; j < intervals.size(); j++){
-                if(intervals[j][0] > end)
-                    break;
-                else{
-                    end = max(end, intervals[j][1]);
-                }
+            if(ans.empty() || ans.back()[1] < intervals[i][0])
+                ans.push_back(intervals[i]);
+            else{
+                ans.back()[1] = max(ans.back()[1], intervals[i][1]);
             }
-            ans.push_back({start, end});
         }
         return ans;
     }
