c523230d0978e4b113807b5ee716d6dbc9d37e2b
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-03-06 16:34:35 +0530

    Time: 0 ms (100%), Space: 17.8 MB (22.79%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int maxProduct(vector<int>& nums) {
-        if(nums.size() == 0)
-            return 0;
-        int current_max = nums[0];
-        int current_min = nums[0];
-        int result = nums[0];
-
-        for (size_t i = 1; i < nums.size(); ++i) {
-            int num = nums[i];
-
-            // Store current_max before updating it, since current_min will use the old value
-            int temp_max = max({num, current_max * num, current_min * num});
-            int temp_min = min({num, current_max * num, current_min * num});
-
-            current_max = temp_max;
-            current_min = temp_min;
-
-            result = max(result, current_max);
+        int maxi = INT_MIN;
+        int prefix = 1, suffix = 1;
+        int n = nums.size();
+        for(int i = 0; i < n; i++){
+            if(prefix == 0)
+                prefix = 1;
+            if(suffix == 0)
+                suffix = 1;
+            prefix *= nums[i];
+            suffix *= nums[n - i - 1];
+            maxi = max(maxi, max(prefix, suffix));
         }
-        return result;
+        return maxi;
     }
 };
