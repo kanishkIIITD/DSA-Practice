3756014c4ecca5fe4f8e9c9ccff5dc59134531e3
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-03-16 23:08:28 +0530

    Time: 0 ms (100%), Space: 26.3 MB (6.49%) - LeetHub

--- 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.cpp
@@ -1,66 +1,34 @@
 class Solution {
 public:
     int singleNonDuplicate(vector<int>& nums) {
-        // int s = 0;
-        // int e = nums.size()-1;
-        // int m = s + (e-s)/2;
-        // while(s < e){
-        //     if(m%2 == 1)
-        //         m--;
-        //     if(nums[m] == nums[m+1])
-        //         s = m+2;
-        //     else
-        //         e = m;
-        //     m = s + (e-s)/2;
-        // }
-        // return nums[m];
-
-        // Solution 2:
-        // int s = 0; 
-        // int e = nums.size()-1;
-        // while(s <= e){
-        //     if(s == e)
-        //         return nums[s];
-        //     int m = s + (e-s)/2;
-        //     if(m%2 == 0){
-        //         if(nums[m] == nums[m+1])
-        //             s = m+2;
-        //         else
-        //             e = m;
-        //     }
-        //     else{
-        //         if(nums[m] == nums[m-1])
-        //             s = m+1;
-        //         else
-        //             e = m-1;
-        //     }
-        // }
-        // return -1;
-
         int n = nums.size();
-        if(n == 1)
-            return nums[0];
-        if(nums[0] != nums[1])
-            return nums[0];
-        if(nums[n-2] != nums[n-1])
-            return nums[n-1];
-        int s = 1;
-        int e = n-2;
-        while(s <= e){
-            int m = s + (e-s)/2;
-            if(nums[m] != nums[m-1] && nums[m] != nums[m+1])
-                return nums[m];
-            if(nums[m] == nums[m+1]){
-                if(m % 2 == 0)
-                    s = m+2;
+        
+        // Edge case: If the array has only 1 element
+        if (n == 1) return nums[0];
+        
+        // Edge case: If the single element is at the very beginning
+        if (nums[0] != nums[1]) return nums[0];
+        
+        // Edge case: If the single element is at the very end
+        if (nums[n - 1] != nums[n - 2]) return nums[n - 1];
+
+        int low = 0, high = n-1;
+        int mid;
+        while(low <= high){
+            mid = (low + high) / 2;
+            if(nums[mid] != nums[mid-1] && nums[mid] != nums[mid+1])
+                return nums[mid];
+            else if(mid % 2 == 0){
+                if(nums[mid-1] == nums[mid])
+                    high = mid-1;
                 else
-                    e = m-1;
+                    low = mid+1;
             }
-            else{
-                if(m % 2 == 0)
-                    e = m-2;
+            else if(mid % 2 == 1){
+                if(nums[mid-1] == nums[mid])
+                    low = mid+1;
                 else
-                    s = m+1;
+                    high = mid-1;
             }
         }
         return -1;
