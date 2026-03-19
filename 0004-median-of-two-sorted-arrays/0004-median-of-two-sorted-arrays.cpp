fc519b8fe22fd1f92e1e427c591efc854197eccc
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-03-19 15:41:22 +0530

    Time: 0 ms (100%), Space: 95 MB (80.33%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,33 +1,29 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
-        if(n1 > n2)
+        int n1 = nums1.size(), n2 = nums2.size();
+        if (n1 > n2) {
             return findMedianSortedArrays(nums2, nums1);
-        int left = (n1 + n2 + 1) / 2;
-        int low = 0;
-        int high = n1;
+        }
+        int low = 0, high = n1, mid1, mid2;
+        int l1, l2, r1, r2;
         while(low <= high){
-            int mid1 = (low + high) / 2;
-            int mid2 = left - mid1;
-            int l1 = INT_MIN;
-            int l2 = INT_MIN;
-            int r1 = INT_MAX;
-            int r2 = INT_MAX;
-            if(mid1 < n1) r1 = nums1[mid1];
-            if(mid2 < n2) r2 = nums2[mid2];
-            if(mid1-1 >= 0) l1 = nums1[mid1-1];
-            if(mid2-1 >= 0) l2 = nums2[mid2-1];
-
-            if(l1 <= r2 && l2 <= r1){
-                return (n1+n2)%2==0 ? ((double)max(l1, l2) + (double)min(r1, r2)) / 2 : max(l1, l2);
-            }
+            mid1 = low + (high - low) / 2;
+            mid2 = (n1 + n2 + 1) / 2 - mid1;
+            l1 = (mid1 == 0) ? INT_MIN : nums1[mid1-1];
+            l2 = (mid2 == 0) ? INT_MIN : nums2[mid2-1];
+            r1 = (mid1 == n1) ? INT_MAX : nums1[mid1];
+            r2 = (mid2 == n2) ? INT_MAX : nums2[mid2];
+            if(l1 <= r2 && l2 <= r1)
+                break;
             else if(l1 > r2)
                 high = mid1-1;
             else
                 low = mid1+1;
         }
-        return 0;
+        if((n1 + n2) % 2 == 0){
+            return ((max(l1, l2) + min(r1, r2)) / 2.0);
+        }
+        return max(l1, l2);
     }
 };
