cf6c94a50fda0ead86adeed38785a83a01c49cc4
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-03-21 15:42:32 +0530

    Time: 8 ms (96.15%), Space: 25.1 MB (59.93%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -8,7 +8,7 @@ public:
             mp[curr].push_back(strs[i]);
         }
         vector<vector<string>> ans;
-        for(auto it: mp)
+        for(auto& it: mp)
             ans.push_back(it.second);
         return ans;
     }
