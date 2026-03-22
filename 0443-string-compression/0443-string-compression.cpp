b94936ed26e78f74784153a4cbe34910b63b0132
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-03-22 17:03:02 +0530

    Time: 0 ms (100%), Space: 13.8 MB (73.15%) - LeetHub

--- 0443-string-compression/0443-string-compression.cpp
+++ 0443-string-compression/0443-string-compression.cpp
@@ -1,60 +1,33 @@
 class Solution {
 public:
     int compress(vector<char>& chars) {
-        int n = chars.size();
-        if (n <= 1)
-            return n;
-        int index = 0;
-        int prev = 0;
-        int i = 1;
-        int count = 1;
-        while (i < n) {
-            if (chars[prev] == chars[i]) {
+        if (chars.empty()) return 0;
+
+        int index = 0, prev = 0, i = 1, count = 1;
+        while(i < chars.size()){
+            if(chars[prev] == chars[i]){
                 count++;
-                prev++;
-                i++;
-            } else if (chars[prev] != chars[i] && count > 1) {
-                vector<int> digits;
-                while (count) {
-                    digits.push_back(count % 10);
-                    count = count / 10;
-                }
-                reverse(digits.begin(), digits.end());
-                chars[index] = chars[prev];
-                index++;
-                for (int j = 0; j < digits.size(); j++) {
-                    chars[index] = digits[j] + '0';
-                    index++;
-                }
-                count = 1;
-                prev++;
-                i++;
-            } else {
-                chars[index] = chars[prev];
-                index++;
-                prev++;
-                i++;
             }
+            else{
+                chars[index++] = chars[prev];
+                if(count > 1){
+                    string countStr = to_string(count);
+                    for(auto c: countStr)
+                        chars[index++] = c;
+                    count = 1;
+                }
+            }
+            prev++;
+            i++;
         }
 
-        chars[index] = chars[prev];
-        index++;
-        if (count > 1) {
-            vector<int> digits;
-            while (count) {
-                digits.push_back(count % 10);
-                count = count / 10;
-            }
-            reverse(digits.begin(), digits.end());
-
-            for (int j = 0; j < digits.size(); j++) {
-                chars[index] = digits[j] + '0';
-                index++;
-            }
+        chars[index++] = chars[prev];
+        if(count > 1){
+            string countStr = to_string(count);
+            for(auto c: countStr)
+                chars[index++] = c;
         }
 
-        for (int i = 0; i < n; i++)
-            cout << chars[i];
         return index;
     }
 };
