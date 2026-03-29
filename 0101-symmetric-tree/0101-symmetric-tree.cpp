7aee5c13744f75556288764eadc74c3af57a84c2
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-03-29 13:55:37 +0530

    Time: 0 ms (100%), Space: 18.4 MB (59.27%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -14,10 +14,11 @@ public:
     bool isSymmetricHelp(TreeNode* left, TreeNode* right){
         if(!left || !right)
             return left == right;
-        if(left->val != right->val) return false;
+        if(left->val != right->val)
+            return false;
         return isSymmetricHelp(left->left, right->right) && isSymmetricHelp(left->right, right->left);
     }
     bool isSymmetric(TreeNode* root) {
-        return !root || isSymmetricHelp(root->left, root->right);
+        return root == NULL || isSymmetricHelp(root->left, root->right);
     }
 };
