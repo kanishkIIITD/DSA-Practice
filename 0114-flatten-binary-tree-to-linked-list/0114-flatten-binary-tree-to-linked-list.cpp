0f4960b5359c084ca325de2b7ee80a68aef27c24
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-03-30 17:35:51 +0530

    Time: 0 ms (100%), Space: 17.5 MB (51.66%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -11,23 +11,16 @@
  */
 class Solution {
 public:
+    void solve(TreeNode* root, TreeNode*& prev){
+        if(!root) return;
+        solve(root->right, prev);
+        solve(root->left, prev);
+        root->right = prev;
+        root->left = NULL;
+        prev = root;
+    }
     void flatten(TreeNode* root) {
-        TreeNode* curr = root;
-        while (curr != nullptr) {
-            if (curr->left != nullptr) {
-                TreeNode* prev = curr->left;
-                // Find the rightmost node in the left subtree
-                while (prev->right != nullptr) {
-                    prev = prev->right;
-                }
-                // Attach the original right subtree to the rightmost node
-                prev->right = curr->right;
-                // Move left subtree to right
-                curr->right = curr->left;
-                curr->left = nullptr;
-            }
-            // Move to the next node
-            curr = curr->right;
-        }
+        TreeNode* prev = NULL;
+        solve(root, prev);
     }
 };
