19ca23c22922914ae7388d7384e4c63f504dcd5c
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-02 21:51:31 +0530

    Time: 59 ms (57.06%), Space: 21.3 MB (42.89%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        set<string> st;
+        unordered_set<string> st;
         for(auto word: wordList)
             st.insert(word);
         if(st.find(endWord) == st.end())
