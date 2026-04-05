8af149f811850d0f6a669415b0a8e9ce7f567464
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-05 16:27:27 +0530

    Time: 3 ms (34.54%), Space: 15.2 MB (19.77%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,23 +1,34 @@
 class Solution {
 public:
-    int solve(int i, int j, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp){
-        if(j < 0 || j >= n)
-            return 1e9;
-        if(i == n-1)
-            return matrix[i][j];
-        if(dp[i][j] != 1e9)
-            return dp[i][j];
-        int left = matrix[i][j] + solve(i+1, j-1, n, matrix, dp);
-        int down = matrix[i][j] + solve(i+1, j, n, matrix, dp);
-        int right = matrix[i][j] + solve(i+1, j+1, n, matrix, dp);
-        return dp[i][j] = min({left, down, right});
-    }
+    // int solve(int i, int j, int n, vector<vector<int>>& matrix, vector<vector<int>>& dp){
+    //     if(j < 0 || j >= n)
+    //         return 1e9;
+    //     if(i == n-1)
+    //         return matrix[i][j];
+    //     if(dp[i][j] != 1e9)
+    //         return dp[i][j];
+    //     int left = matrix[i][j] + solve(i+1, j-1, n, matrix, dp);
+    //     int down = matrix[i][j] + solve(i+1, j, n, matrix, dp);
+    //     int right = matrix[i][j] + solve(i+1, j+1, n, matrix, dp);
+    //     return dp[i][j] = min({left, down, right});
+    // }
+
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int mini = INT_MAX;
         int n = matrix.size();
         vector<vector<int>> dp(n+1, vector<int>(n+1, 1e9));
+        dp[n-1] = matrix[n-1];
+        for(int i = n-2; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                int down = dp[i+1][j];
+                int left = (j > 0) ? dp[i+1][j-1] : 1e9;
+                int right = (j < n-1) ? dp[i+1][j+1] : 1e9;
+                
+                dp[i][j] = matrix[i][j] + min({left, down, right});
+            }
+        }
         for(int i = 0; i < n; i++)
-            mini = min(mini, solve(0, i, n, matrix, dp));
+            mini = min(mini, dp[0][i]);
         return mini;
     }
 };
