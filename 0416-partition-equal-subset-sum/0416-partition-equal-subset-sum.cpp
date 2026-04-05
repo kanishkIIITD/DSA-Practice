5880b03ad904f650314155957ef674ae921735c3
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-05 22:39:08 +0530

    Time: 95 ms (70.18%), Space: 110.2 MB (44.76%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,41 +1,28 @@
 class Solution {
 public:
-    bool solve(int index, int target, vector<int>&nums, vector<vector<int>> &dp){
-        if(target == 0)
-            return true;
-        if(index == 0)
-            return nums[0] == target;
-        
-        if(dp[index][target] != -1)
-            return dp[index][target];
-
-        bool notTake = solve(index-1, target, nums, dp);
-        bool take = target >= nums[index] ? solve(index-1, target - nums[index], nums, dp) : false;
-        return dp[index][target] = take || notTake;
+    bool isSubsetSum(vector<int>& arr, int sum) {
+        int n = arr.size();
+        vector<int> dp(sum+1);
+        dp[0] = 1;
+        for(int i = n-1; i >= 0; i--){
+            vector<int> temp(sum+1);
+            temp[0] = 1;
+            for(int j = 1; j <= sum; j++){
+                bool pick = false;
+                if(j-arr[i] >= 0)
+                    pick = dp[j-arr[i]];
+                bool notpick = dp[j];
+                temp[j] = pick || notpick;
+            }
+            dp = temp;
+        }
+        return dp[sum];
     }
     bool canPartition(vector<int>& nums) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int n = nums.size();
-        if(sum % 2 != 0)
-            return false;
-        // vector<vector<int>> dp(n, vector<int>(sum/2+1, -1));
-        // return solve(n-1, sum/2, nums, dp);
-
-        // vector<vector<int>> dp(n, vector<int>(sum/2+1, 0));
-        vector<int> dp(sum/2+1, 0);
-        vector<int> prev(sum/2+1, 0);
-        dp[0] = 1;
-        prev[0] = 1;
-        if(nums[0] <= sum/2)
-            dp[nums[0]] = true;
-        for(int index = 1; index < n; index++){
-            for(int target = 1; target < sum/2+1; target++){
-                bool notTake = prev[target];
-                bool take = target >= nums[index] ? prev[target - nums[index]] : false;
-                dp[target] = take || notTake;
-            }
-            prev = dp;
-        }
-        return prev[sum/2];
+        int totalSum = 0;
+        for(auto num: nums) totalSum += num;
+        if(totalSum % 2 == 1) return false;
+        int target = totalSum / 2;
+        return isSubsetSum(nums, target);
     }
 };
