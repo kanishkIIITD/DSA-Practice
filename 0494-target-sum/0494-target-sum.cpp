038524e0c9f39d5820df7f526d2a02bc8ccc9f22
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-05 22:28:20 +0530

    Time: 950 ms (5.9%), Space: 11.5 MB (92.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    void computeSums(const vector<int>& nums, int start, int end,  vector<long long>& sums) {
-        sums.push_back(0);
-        for (int i = start; i < end; ++i) {
-            int num = nums[i];
-            int n = sums.size();
-            for (int j = 0; j < n; ++j) {
-                sums.push_back(sums[j] + num);
-                sums[j] -= num;
-            }
-        }
+    int solve(int i, int target, vector<int>& nums){
+        if(i == nums.size() && target == 0)
+            return 1;
+        if(i >= nums.size())
+            return 0;
+        int sub = solve(i+1, target+nums[i], nums);
+        int add = solve(i+1, target-nums[i], nums);
+        return sub + add;
     }
-
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int mid = n / 2;
-        vector<long long> sums1;
-        vector<long long> sums2;
-        computeSums(nums, 0, mid, sums1);
-        computeSums(nums, mid, n, sums2);
-        unordered_map<long long, long long> countMap;
-        for (auto sum : sums2) {
-            countMap[sum]++;
-        }
-        long long total = 0;
-        for (auto sum : sums1) {
-            long long complement = (long long)target - sum;
-            if (countMap.find(complement) != countMap.end()) {
-                total += countMap[complement];
-            }
-        }
-
-        return total;
+        return solve(0, target, nums);
     }
 };
