4fc466d9b3f70de0985ac88bce65b31a5d6a979e
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-05 13:23:36 +0530

    Time: 2 ms (19.83%), Space: 9.3 MB (64.88%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.cpp
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -1,72 +1,16 @@
 class Solution {
 public:
-    // int solve(int i, int j, vector<vector<int>>&vis, vector<vector<int>>&dp){
-    //     int m = vis.size();
-    //     int n = vis[0].size();
-    //     if(i == m-1 && j == n-1){
-    //         return 1;
-    //     }
-
-    //     if(dp[i][j] != -1){
-    //         return dp[i][j];
-    //     }
-        
-    //     vis[i][j] = 1;
-
-    //     int paths = 0;
-
-    //     vector<int> dx = {0, 1};
-    //     vector<int> dy = {1, 0};
-    //     for(int k = 0; k < 2; k++){
-    //         int nr = i + dx[k];
-    //         int nc = j + dy[k];
-    //         if(0 <= nr && nr < m && 0 <= nc && nc < n && !vis[nr][nc]){
-    //             paths += solve(nr, nc, vis, dp);
-    //         }
-    //     }
-    //     vis[i][j] = 0;
-    //     return dp[i][j] = paths;
-    // }
+    int solve(int i, int j, int m, int n, vector<vector<int>>& dp){
+        if(i == m-1 && j == n-1)
+            return dp[i][j] = 1;
+        else if(i >= m || j >= n)
+            return dp[i][j] = 0;
+        if(dp[i][j] != -1)
+            return dp[i][j];
+        return dp[i][j] = solve(i+1, j, m, n, dp) + solve(i, j+1, m, n, dp);
+    }
     int uniquePaths(int m, int n) {
-        // vector<vector<int>> vis(m, vector<int>(n, 0));
-        // vector<vector<int>> dp(m, vector<int>(n, 0));
-        // // int count = 0;
-        // // count = solve(0, 0, vis, dp);
-        // // return count;
-
-        // // dp[m-1][n-1] = 1;
-        // // vis[m-1][n-1] = 1;
-        // for(int i = m-1; i >= 0; i--){
-        //     for(int j = n-1; j >= 0; j--){
-        //         if(i == m-1 && j == n-1)
-        //             continue;
-        //         // vis[i][j] = 1;
-
-        //         int paths = 0;
-
-        //         vector<int> dx = {0, 1};
-        //         vector<int> dy = {1, 0};
-        //         for(int k = 0; k < 2; k++){
-        //             int nr = i + dx[k];
-        //             int nc = j + dy[k];
-        //             if(0 <= nr && nr < m && 0 <= nc && nc < n){
-        //                 paths += dp[nr][nc];
-        //             }
-        //         }
-        //         // vis[i][j] = 0;
-        //         dp[i][j] = paths;
-        //     }
-        // }
-        // return dp[0][0];
-
-        vector<int> dp(n, 1); // Initialize with 1's for first row
-    
-        for(int i = 1; i < m; i++) {       // Start from second row
-            for(int j = 1; j < n; j++) {   // Start from second column
-                dp[j] += dp[j-1]; // dp[j] = top (old dp[j]) + left (dp[j-1])
-            }
-        }
-        
-        return dp[n-1];
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return solve(0, 0, m, n, dp);
     }
 };
