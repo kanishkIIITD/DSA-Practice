1ce7719c8b411b3defb7d499dd33fa1565de2d2d
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-05 14:28:48 +0530

    Time: 0 ms (100%), Space: 11.9 MB (52.53%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,18 +1,32 @@
 class Solution {
 public:
-    int solve(int i, int j, int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
-        if(i == m-1 && j == n-1 && obstacleGrid[i][j] == 0)
-            return 1;
-        if(i >= m || j >= n || obstacleGrid[i][j] == 1)
-            return 0;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        return dp[i][j] = solve(i+1, j, m, n, obstacleGrid, dp) + solve(i, j+1, m, n, obstacleGrid, dp);
-    }
+    // int solve(int i, int j, int m, int n, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp){
+    //     if(i == m-1 && j == n-1 && obstacleGrid[i][j] == 0)
+    //         return 1;
+    //     if(i >= m || j >= n || obstacleGrid[i][j] == 1)
+    //         return 0;
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     return dp[i][j] = solve(i+1, j, m, n, obstacleGrid, dp) + solve(i, j+1, m, n, obstacleGrid, dp);
+    // }
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return solve(0, 0, m, n, obstacleGrid, dp);
+        vector<vector<long long>> dp(m+1, vector<long long>(n+1));
+        // return solve(0, 0, m, n, obstacleGrid, dp);
+        if(obstacleGrid[m-1][n-1] == 1)
+            return 0;
+        dp[m-1][n-1] = 1;
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(i == m-1 && j == n-1)
+                    continue;
+                if(obstacleGrid[i][j] == 1)
+                    dp[i][j] = 0;
+                else
+                    dp[i][j] = dp[i+1][j] + dp[i][j+1];
+            }
+        }
+        return dp[0][0];
     }
 };
