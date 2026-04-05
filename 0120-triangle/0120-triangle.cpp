4c36d6806dab589386fb182a866e79b3e8b2f624
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-05 15:44:43 +0530

    Time: 7 ms (3.54%), Space: 13.3 MB (50.13%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,16 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp){
-        if(i >= triangle.size())
-            return 0;
-        if(dp[i][j] != 1e9)
-            return dp[i][j];
-        return dp[i][j] = triangle[i][j] + min(solve(i+1, j, triangle, dp), solve(i+1, j+1, triangle, dp));
-    }
+    // int solve(int i, int j, vector<vector<int>>& triangle, vector<vector<int>>& dp){
+    //     if(i >= triangle.size())
+    //         return 0;
+    //     if(dp[i][j] != 1e9)
+    //         return dp[i][j];
+    //     return dp[i][j] = triangle[i][j] + min(solve(i+1, j, triangle, dp), solve(i+1, j+1, triangle, dp));
+    // }
     int minimumTotal(vector<vector<int>>& triangle) {
         int m = triangle.size();
         int n = triangle[m-1].size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, 1e9));
-        return solve(0, 0, triangle, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        // return solve(0, 0, triangle, dp);
+        for(int i = n-1; i >= 0; i--){
+            for(int j = triangle[i].size()-1; j >= 0; j--){
+                dp[i][j] = triangle[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
+            }
+        }
+        return dp[0][0];
     }
 };
