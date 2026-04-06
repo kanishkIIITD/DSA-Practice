8086eec9a9d99f535a0fe9c7ca1a6dde6e5cfdcc
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-06 15:08:47 +0530

    Time: 43 ms (28.88%), Space: 60.9 MB (16.56%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.cpp
+++ 0518-coin-change-ii/0518-coin-change-ii.cpp
@@ -1,22 +1,36 @@
 class Solution {
 public:
-    int solve(int i, int amount, vector<int>& coins, vector<vector<int>>& dp){
-        if(i == coins.size()){
-            if(amount == 0) 
-                return 1;
-            return 0;
-        }
-        if(dp[i][amount] != -1)
-            return dp[i][amount];
-        int notpick = solve(i+1, amount, coins, dp);
-        int pick = 0;
-        if(amount >= coins[i])
-            pick = solve(i, amount - coins[i], coins, dp);
-        return dp[i][amount] = pick + notpick;
-    }
+    // int solve(int i, int amount, vector<int>& coins, vector<vector<int>>& dp){
+    //     if(i == coins.size()){
+    //         if(amount == 0) 
+    //             return 1;
+    //         return 0;
+    //     }
+    //     if(dp[i][amount] != -1)
+    //         return dp[i][amount];
+    //     int notpick = solve(i+1, amount, coins, dp);
+    //     int pick = 0;
+    //     if(amount >= coins[i])
+    //         pick = solve(i, amount - coins[i], coins, dp);
+    //     return dp[i][amount] = pick + notpick;
+    // }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(n+1, vector<int>(amount+1, -1));
-        return solve(0, amount, coins, dp);
+        // vector<vector<int>> dp(n+1, vector<int>(amount+1));
+        vector<unsigned long long> dp(amount+1);
+        // return solve(0, amount, coins, dp);
+        dp[0] = 1;
+        for(int i = n-1; i >= 0; i--){
+            vector<unsigned long long> temp(amount+1);
+            for(int j = 0; j <= amount; j++){
+                unsigned long long notpick = dp[j];
+                unsigned long long pick = 0;
+                if(j >= coins[i])
+                    pick = temp[j-coins[i]];
+                temp[j] = pick + notpick;
+            }
+            dp = temp;
+        }
+        return dp[amount];
     }
 };
