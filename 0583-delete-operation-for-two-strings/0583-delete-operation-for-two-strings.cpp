d853751ffc663ef27873035bca20ba99d1d31b80
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-06 23:55:10 +0530

    Time: 18 ms (20.11%), Space: 16.7 MB (56.32%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,20 +1,33 @@
 class Solution {
 public:
-    int solve(int i, int j, string& word1, string& word2, int m, int n, vector<vector<int>>& dp){
-        if(i == m)
-            return n - j;
-        if(j == n)
-            return m - i;
-        if(dp[i][j] != -1)
-            return dp[i][j];
-        if(word1[i] == word2[j])
-            return dp[i][j] = solve(i+1, j+1, word1, word2, m, n, dp);
-        else
-            return dp[i][j] = 1 + min(solve(i, j+1, word1, word2, m, n, dp), solve(i+1, j, word1, word2, m, n, dp));
-    }
+    // int solve(int i, int j, string& word1, string& word2, int m, int n, vector<vector<int>>& dp){
+    //     if(i == m)
+    //         return n - j;
+    //     if(j == n)
+    //         return m - i;
+    //     if(dp[i][j] != -1)
+    //         return dp[i][j];
+    //     if(word1[i] == word2[j])
+    //         return dp[i][j] = solve(i+1, j+1, word1, word2, m, n, dp);
+    //     else
+    //         return dp[i][j] = 1 + min(solve(i, j+1, word1, word2, m, n, dp), solve(i+1, j, word1, word2, m, n, dp));
+    // }
     int minDistance(string word1, string word2) {
         int m = word1.size(), n = word2.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
-        return solve(0, 0, word1, word2, m, n, dp);
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+        for(int i = 0; i < m; i++)
+            dp[i][n] = m - i;
+        for(int j = 0; j < n; j++)
+            dp[m][j] = n - j;
+        for(int i = m-1; i >= 0; i--){
+            for(int j = n-1; j >= 0; j--){
+                if(word1[i] == word2[j])
+                    dp[i][j] = dp[i+1][j+1];
+                else
+                    dp[i][j] = 1 + min(dp[i][j+1], dp[i+1][j]);
+            }
+        }
+        return dp[0][0];
+        // return solve(0, 0, word1, word2, m, n, dp);
     }
 };
