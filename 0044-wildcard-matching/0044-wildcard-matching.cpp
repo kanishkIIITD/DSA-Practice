5918feeb488f11c852cefe2547962a6743964631
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-07 13:33:33 +0530

    Time: 58 ms (40.21%), Space: 52.3 MB (36.96%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -22,8 +22,9 @@ public:
     // }
     bool isMatch(string s, string p) {
         int m = s.size(), n = p.size();
-        vector<vector<int>> dp(m+1, vector<int>(n+1));
-        dp[m][n] = 1;
+        // vector<vector<int>> dp(m+1, vector<int>(n+1));
+        vector<int> dp(n+1);
+        dp[n] = 1;
         for(int j = 0; j < n; j++){
             bool flag = true;;
             for(int k = j; k < p.size(); k++){
@@ -33,17 +34,19 @@ public:
                 }
             }
             if(flag == true)
-                dp[m][j] = 1;
+                dp[j] = 1;
         }
         for(int i = m-1; i >= 0; i--){
+            vector<int> temp(n+1);
             for(int j = n-1; j >= 0; j--){
                 if(s[i] == p[j] || p[j] == '?')
-                    dp[i][j] = dp[i+1][j+1];
+                    temp[j] = dp[j+1];
                 else if(p[j] == '*')
-                    dp[i][j] = dp[i][j+1] || dp[i+1][j];
+                    temp[j] = temp[j+1] || dp[j];
             }
+            dp = temp;
         }
-        return dp[0][0];
+        return dp[0];
         // return solve(0, 0, s, p, dp);
     }
 };
