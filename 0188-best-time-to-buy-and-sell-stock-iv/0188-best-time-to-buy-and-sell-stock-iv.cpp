c0a31a4af0f07f8734b4153072055b397f3ae9c8
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-07 23:29:11 +0530

    Time: 7 ms (58.31%), Space: 18.1 MB (46.46%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,40 +1,18 @@
 class Solution {
 public:
-    // int solve(int index, int buy, int k, vector<int>&prices, vector<vector<vector<int>>> &dp){
-    //         if(k == 0)
-    //             return 0;
-    //         if(index == prices.size())
-    //             return 0;
-    //         if(dp[index][buy][k] != -1)
-    //             return dp[index][buy][k] ;
-    //         if(buy){
-    //             return dp[index][buy][k] = max(-prices[index] + solve(index+1, 0, k, prices, dp), solve(index+1, 1, k, prices, dp));
-    //         }   
-    //         else{
-    //             return dp[index][buy][k] = max(prices[index] + solve(index+1, 1, k-1, prices, dp), solve(index+1, 0, k, prices, dp));
-    //         }      
-    //     }
-    int maxProfit(int kmax, vector<int>& prices) {
+    int maxProfit(int k, vector<int>& prices) {
         int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1, -1)));
-        // return solve(0, 1, k, prices, dp);
-
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(kmax+1, 0)));
-        vector<vector<int>> dp(2, vector<int>(kmax+1, 0));
-        vector<vector<int>> next(2, vector<int>(kmax+1, 0));
-        for(int index = n-1; index >= 0; index--){
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(k+1)));
+        for(int i = n-1; i >= 0; i--){
             for(int buy = 0; buy < 2; buy++){
-                for(int k = 1; k < kmax+1; k++){
-                    if(buy){
-                        dp[buy][k] = max(-prices[index] + next[0][k], next[1][k]);
-                    }   
-                    else{
-                        dp[buy][k] = max(prices[index] + next[1][k-1], next[0][k]);
-                    }
+                for(int cap = 1; cap < k+1; cap++){
+                    if(buy)
+                        dp[i][buy][cap] = max(dp[i+1][buy][cap], -prices[i] + dp[i+1][!buy][cap]);
+                    else
+                        dp[i][buy][cap] = max(dp[i+1][buy][cap], prices[i] + dp[i+1][!buy][cap-1]);
                 }
             }
-            next = dp;
         }
-        return next[1][kmax];
+        return dp[0][1][k];
     }
 };
