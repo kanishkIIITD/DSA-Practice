4f462b27221bdd5876e82d4ce9f748d600b4fcc9
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-04-08 12:47:03 +0530

    Time: 3 ms (34.6%), Space: 17.5 MB (5.21%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,46 +1,23 @@
 class Solution {
 public:
-    // int solve(int index, int buy, int cooldown, vector<int>&prices, vector<vector<vector<int>>> &dp){
-    //     if(index == prices.size())
-    //         return 0;
-        
-    //     if(dp[index][buy][cooldown] != -1)
-    //         return dp[index][buy][cooldown];
-
-    //     if(buy){
-    //         if(cooldown){
-    //             return dp[index][buy][cooldown] = solve(index+1, 1, 0, prices, dp);
-    //         }
-    //         else
-    //             return dp[index][buy][cooldown] = max(-prices[index] + solve(index+1, 0, 0, prices, dp), solve(index+1, 1, 0, prices, dp));
-    //     }
-    //     else
-    //         return dp[index][buy][cooldown] = max(prices[index] + solve(index+1, 1, 1, prices, dp), solve(index+1, 0, cooldown, prices, dp));
-    // }
+    int solve(int i, bool buy, bool cooldown, vector<int>& prices, vector<vector<vector<int>>> &dp){
+        if(i == prices.size())
+            return 0;
+        if(dp[i][buy][cooldown] != -1)
+            return dp[i][buy][cooldown];
+        if(cooldown){
+            return dp[i][buy][cooldown] = solve(i+1, buy, !cooldown, prices, dp);
+        }
+        else if(buy){
+            return dp[i][buy][cooldown] = max(solve(i+1, buy, cooldown, prices, dp), -prices[i] + solve(i+1, !buy, cooldown, prices, dp));
+        }
+        else{
+            return dp[i][buy][cooldown] = max(solve(i+1, buy, cooldown, prices, dp), prices[i] + solve(i+1, !buy, !cooldown, prices, dp));
+        }
+    }
     int maxProfit(vector<int>& prices) {
         int n = prices.size();
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(2, -1)));
-        // return solve(0, 1, 0, prices, dp); //{index, buy, cooldown}
-
-        // vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(2, 0)));
-        vector<vector<int>> dp(2, vector<int>(2, 0));
-        vector<vector<int>> next(2, vector<int>(2, 0));
-        for(int index = n-1; index >= 0; index--){
-            for(int buy = 0; buy < 2; buy++){
-                for(int cooldown = 1; cooldown >= 0; cooldown--){
-                    if(buy){
-                        if(cooldown){
-                            dp[buy][cooldown] = next[1][0];
-                        }
-                        else
-                            dp[buy][cooldown] = max(-prices[index] + next[0][0], next[1][0]);
-                    }
-                    else
-                        dp[buy][cooldown] = max(prices[index] + next[1][1], next[0][cooldown]);
-                }
-            }
-            next = dp;
-        }
-        return next[1][0];
+        vector<vector<vector<int>>> dp(n+1, vector<vector<int>>(2, vector<int>(2, -1)));
+        return solve(0, true, false, prices, dp);
     }
 };
