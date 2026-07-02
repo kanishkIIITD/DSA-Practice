56dc7700bc00262b563794ef88d1f722e3cc0470
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-07-02 15:01:08 +0530

    Time: 13 ms (76.58%), Space: 32 MB (57.32%) - LeetHub

--- 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
+++ 3286-find-a-safe-walk-through-a-grid/3286-find-a-safe-walk-through-a-grid.cpp
@@ -1,37 +1,28 @@
 class Solution {
 public:
-    struct help{
-        int row;
-        int col;
-        int heal;
-    };
     bool findSafeWalk(vector<vector<int>>& grid, int health) {
         int m = grid.size(), n = grid[0].size();
-        queue<help> q;
-        if(grid[0][0] == 1)
-            health--;
-        if (health <= 0) return false;
-        q.push({0, 0, health});
-        vector<vector<int>> vis(m, vector<int>(n, -1));
-        vis[0][0] = health;
+        vector<vector<int>> dist(m, vector<int>(n, 1e9));
+        deque<pair<int, int>> dq;
+        dist[0][0] = grid[0][0];
+        dq.push_back({0, 0});
         int dr[4] = {-1, 1, 0, 0};
         int dc[4] = {0, 0, -1, 1};
-        while(!q.empty()){
-            auto front = q.front();
-            q.pop();
-            int r = front.row;
-            int c = front.col;
-            int h = front.heal;
+        while(!dq.empty()){
+            auto [r, c] = dq.front();
+            dq.pop_front();
             if(r == m-1 && c == n-1)
-                return true;
+                return health - dist[r][c] > 0;
             for(int i = 0; i < 4; i++){
-                int nr = r + dr[i];
-                int nc = c + dc[i];
+                int nr = r + dr[i], nc = c + dc[i];
                 if(0 <= nr && nr < m && 0 <= nc && nc < n){
-                    int nextHealth = h - grid[nr][nc];
-                    if (nextHealth > 0 && nextHealth > vis[nr][nc]) {
-                        vis[nr][nc] = nextHealth;
-                        q.push({nr, nc, nextHealth});
+                    int cost = grid[nr][nc];
+                    if(dist[r][c] + cost < dist[nr][nc]){
+                        dist[nr][nc] = dist[r][c] + cost;
+                        if(cost == 0)
+                            dq.push_front({nr, nc});
+                        else
+                            dq.push_back({nr, nc});
                     }
                 }
             }
