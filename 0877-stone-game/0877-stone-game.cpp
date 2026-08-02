3139aad8ff0cd9d6c0fd02424ad306bbd6c7e1a3
Kanishk Singh <53936518+kanishkIIITD@users.noreply.github.com>
2026-08-02 16:26:20 +0530

    Time: 11 ms (27.05%), Space: 19.6 MB (27.79%) - LeetHub

--- 0877-stone-game/0877-stone-game.cpp
+++ 0877-stone-game/0877-stone-game.cpp
@@ -2,17 +2,17 @@ class Solution {
 public:
     int solve(int i, int j, vector<int>& piles, vector<vector<int>> &dp){
         if(i > j)
-            return -1;
-        if(dp[i][j] != -2)
+            return 0;
+        if(dp[i][j] != -1)
             return dp[i][j];
         int both = solve(i+1, j-1, piles, dp);
-        int first = piles[i] + max(both, solve(i+2, j, piles, dp));
-        int last = piles[j] + max(both, solve(i, j-2, piles, dp));
+        int first = piles[i] + min(both, solve(i+2, j, piles, dp));
+        int last = piles[j] + min(both, solve(i, j-2, piles, dp));
         return dp[i][j] = max(first, last);
     }
     bool stoneGame(vector<int>& piles) {
         int n = piles.size();
-        vector<vector<int>> dp(n, vector<int>(n, -2));
+        vector<vector<int>> dp(n, vector<int>(n, -1));
         int opt = solve(0, n-1, piles, dp);
         int total = 0;
         for(auto p: piles)
